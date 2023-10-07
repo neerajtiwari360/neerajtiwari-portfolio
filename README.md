be3ed42a82fc7d5589507dc0d92a8d5c3d5953f7
Neeraj <34862790+neerajtiwari360@users.noreply.github.com>
2023-10-07 12:41:17 +0530

    init commit.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# neerajtiwari.github.io
-Personal Profile
+A simple landing page - first designed in figma and then coded in React. Hope you like it.
+
+- https://www.youtube.com/watch?v=GVjIflROwJ4&t=1104s
---
+++ src/Components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+          Unlocking Solutions, Empowering Innovation.
+          </h1>
+          <p className="primary-text">
+          I am a seasoned technical consultant with a passion for solving complex challenges and driving innovation. With a strong background in [mention your key expertise areas], I specialize in providing expert guidance and strategic solutions to businesses of all sizes. Let's collaborate to elevate your technical endeavors and achieve your goals.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
