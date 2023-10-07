b72399d4311bd84a6030fd10979062c3ada38e19
Neeraj <34862790+neerajtiwari360@users.noreply.github.com>
2023-10-07 13:13:07 +0530

    init commit.

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -25,7 +25,7 @@ const Work = () => {
     <div className="work-section-wrapper">
       <div className="work-section-top">
         <p className="primary-subheading">Work</p>
-        <h1 className="primary-heading">How It Works</h1>
+        <h1 className="primary-heading">What We Offer</h1>
         <p className="primary-text">
           Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
           elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
